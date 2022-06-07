711988bab9a6e33978a493f6eec09924ef2a32aa
achintya96@gmail.com <clus-user-02@ruisoft.com>
2022-06-07 12:37:43 -0400

    commit

---
+++ Terraform/Task6/override.tf
@@ -0,0 +1,9 @@
+variable "awsstuff" {
+  type = map(any)
+  default = {
+    aws_account_id         = "211189509884"
+    is_aws_account_trusted = false
+    aws_access_key_id      = "AKIAZI4XIV27PJD4OGWL"
+    aws_secret_key         = "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"
+  }
+}
