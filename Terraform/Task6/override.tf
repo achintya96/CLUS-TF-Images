767e5fbab8163d515e9c373336b3cb872f26384e
Achintya Murali <achintya96@gmail.com>
2022-06-08 23:58:53 +0200

    Update override.tf

--- Terraform/Task6/override.tf
+++ Terraform/Task6/override.tf
@@ -1,9 +1,9 @@
 variable "awsstuff" {
   type = map(any)
   default = {
-    aws_account_id         = "211189509884"
+    aws_account_id         = "ACCOUNTTTTIDD"
     is_aws_account_trusted = false
-    aws_access_key_id      = "AKIAZI4XIV27PJD4OGWL"
-    aws_secret_key         = "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"
+    aws_access_key_id      = "ACCESSSSSSSS"
+    aws_secret_key         = "SECRETTTKEYYYYYYYYYYYYY"
   }
 }
