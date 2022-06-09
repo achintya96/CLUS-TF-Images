beebb9ce638b6b076e5ac3a76e14910f85400337
Achintya Murali <achintya96@gmail.com>
2022-06-09 11:30:59 +0200

    Update SanityCheck_Intern.py

--- Terraform/Task6/SanityCheck_Intern.py
+++ Terraform/Task6/SanityCheck_Intern.py
@@ -3,23 +3,22 @@ import boto3
 aws_profile_name = 'tfuser'
 boto3.setup_default_session(profile_name = aws_profile_name)
 ec2 = boto3.client('ec2')
-amis = ec2.describe_images(Owners=[
-        'self'
-    ])
+ec2s =boto3.resource('ec2')
 print ('Script is running.....')
-for ami in amis['Images']:
-    create_date = ami['CreationDate']
-    ami_id = ami['ImageId']
-#    print (ami['ImageId'], ami['CreationDate'])
-#    print ("deleting -> " + ami_id + " - create_date = " + create_date)
-    #deregister the AMI
-    ec2.deregister_image(ImageId=ami_id)
 
+print('Enter Instance ID of backup VM(not any of the 3 used in application):')
+backupec2id = input()
+list=[backupec2id]
 response = ec2.describe_instances()
+
 
 
 for r in response['Reservations']:
 	for i in r['Instances']:
 		print ('EC2 ' + i['InstanceId'] + ' is healthy')
+
+ec2s.instances.filter(InstanceIds=list).terminate()
+
 print('Key Pairs Secure')
 print('Config is backed up')
+
